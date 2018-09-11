cda8d72363173534f5af4f988043fcf33fffef2b
SanderKok <sander.jt.kok@gmail.com>
2018-09-11 11:40:12 +0200

    JavaScript slider commit

---
+++ JavaScript_SlideShow/js/javascript_slideshow.js
@@ -0,0 +1,100 @@
+var timer = null;
+var currentSlide = 0,
+	slides = document.getElementsByTagName("img");
+var slideInterval = setInterval(nextSlide, 3000);
+var playing = true;
+
+	// Previous slide
+function prevSlide() {
+	currentSlide--
+	console.log(currentSlide);
+	
+	if (currentSlide < 0) {
+		currentSlide = 3;
+	} else if (currentSlide === 0) {
+		document.getElementById("slide1").style.display = "block";
+		document.getElementById("slide2").style.display = "none";
+		document.getElementById("slide3").style.display = "none";
+	} else if (currentSlide === 1){
+		document.getElementById("slide1").style.display = "none";
+		document.getElementById("slide2").style.display = "block";
+		document.getElementById("slide3").style.display = "none";
+	} else if (currentSlide === 2){
+		document.getElementById("slide1").style.display = "none";
+		document.getElementById("slide2").style.display = "none";
+		document.getElementById("slide3").style.display = "block";
+	} else {
+		currentSlide = 3;
+	}
+}
+
+	// Next slide
+function nextSlide() {
+	currentSlide++
+	console.log(currentSlide);
+	
+	if (currentSlide > 2) {
+		currentSlide = -1;
+	} else if (currentSlide === 0) {
+		document.getElementById("slide1").style.display = "block";
+		document.getElementById("slide2").style.display = "none";
+		document.getElementById("slide3").style.display = "none";
+	} else if (currentSlide === 1){
+		document.getElementById("slide1").style.display = "none";
+		document.getElementById("slide2").style.display = "block";
+		document.getElementById("slide3").style.display = "none";
+	} else if (currentSlide === 2){
+		document.getElementById("slide1").style.display = "none";
+		document.getElementById("slide2").style.display = "none";
+		document.getElementById("slide3").style.display = "block";
+	} else {
+		currentSlide = 0;
+	}
+}
+
+// Set the var for the pause button
+var pauseButton = document.getElementById("pause");
+
+// Pause  auto-play
+function pauseSlide() {
+	console.log("Paused.");
+	playing = false;
+	clearInterval(slideInterval);
+}
+
+// Resume auto-play
+function playSlide() {
+	console.log("Resumed.");
+	playing = true;
+	slideInterval = setInterval(nextSlide, 3000);
+}
+
+// Add onClick event to the pause button
+// If it is auto playing, it will pause.
+// If it is NOT auto playing, it will start.
+pauseButton.onclick = function() {
+	if(playing) {
+		var text = "Paused.";
+		var boldText = text.bold();
+		document.getElementById("playing-or-paused").innerHTML = boldText;
+		pauseSlide();
+	} else {
+		document.getElementById("playing-or-paused").innerHTML = "";
+		playSlide();
+	}
+}
+
+// Get key pressed
+var keyCode = "";
+
+function getKey(e) {
+	keyCode = e.keyCode
+	
+	if (keyCode === 39) {
+		nextSlide();
+	}
+	if (keyCode === 37) {
+		prevSlide();
+	}
+}
+document.onkeydown = getKey;
